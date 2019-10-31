e7b274d634b809f882a2b73fa9443992fb3664df
zidane mouloud <zizopolo92@gmail.com>
2019-10-31 14:36:38 +0100

    init app

---
+++ client-app/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAe0ScqtiKfcx-8VAA2WGB0jaqp5CK2x2A",
+    authDomain: "map-project-660a8.firebaseapp.com",
+    databaseURL: "https://map-project-660a8.firebaseio.com",
+    projectId: "map-project-660a8",
+    storageBucket: "map-project-660a8.appspot.com",
+    messagingSenderId: "807421239776",
+    appId: "1:807421239776:web:2ccca8785c1745bd"
+  },
+  userApi: 'https://map-project-660a8.firebaseapp.com/api'
+};
